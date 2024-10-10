14599a736c0b19c312fd0aad9e42a17f1b2c046a
Raffi Hotter <raphael.hotter@gmail.com>
2024-10-10 10:25:09 -0400

    Render math

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
       </head>
       <body className={` fade-in`}>
         <div className="">{children}</div>
