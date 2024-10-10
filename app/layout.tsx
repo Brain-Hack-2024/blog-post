9e74112439f3dbe352b3d3f4efc6425fdee602f1
Raffi Hotter <raphael.hotter@gmail.com>
2024-10-10 15:54:10 -0400

    Move AE to /ae

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,8 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous" />
       </head>
       <body className={` fade-in`}>
-        <div className="">{children}</div>
+        <div className="max-w-2xl md:ml-[20vw] ml-0 mt-16 md:mt-24 mb-36 px-4 overflow-clip md:overflow-visible">{children}</div>
       </body>
     </html>
   );
 }
-
-export const metadata = {
-  title: "We have AE at home",
-  description: "The acoustoelectric effect",
-};
