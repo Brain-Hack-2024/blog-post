2f0c70125738490ecee2a206d72d4f70fa6eee39
Raffi Hotter <raphael.hotter@gmail.com>
2024-10-10 21:52:00 -0400

    Add controls to videos

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,6 +1,6 @@
-export default function Video({src, controls=false, sound=false}: {src: string, controls?: boolean, sound?: boolean}) {
+export default function Video({src, sound=false}: {src: string, sound?: boolean}) {
   return (
-    <video loop playsInline className="w-full" controls={controls} muted={!sound} autoPlay={!sound}>
+    <video loop playsInline controls className="w-full" muted={!sound} autoPlay={!sound}>
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
