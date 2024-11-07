2e27440c856dc66c237b7c5b12150bbf0d27ad3d
Raffi Hotter <raphael.hotter@gmail.com>
2024-11-07 15:30:09 -0800

    reduce middleware calls

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+// Define which routes should use this middleware
+export const config = {
+  matcher: '/'
+}
+
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/ae', request.url))
